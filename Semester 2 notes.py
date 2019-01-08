17f4460027b852dff47149098a78fb87978aeefa
Redflare654 <42391767+Redflare654@users.noreply.github.com>
2019-01-08 14:35:15 -0800

    2

---
+++ Semester 2 notes.py
@@ -0,0 +1,10 @@
+print("hello world")
+# how big is the mouse?
+car = 5
+driving = True
+
+print ("i have %d cars" % car)
+print ("i have" + str(car) + "cars.")
+age = input("how old are you?")
+print("really you are this %s old? welp rip" % age)
+list = [blue, red,]
