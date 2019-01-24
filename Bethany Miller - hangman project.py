c81c236daf52e82e33c2c05639b73c9d51c1254c
Redflare654 <42391767+Redflare654@users.noreply.github.com>
2019-01-24 14:37:53 -0800

    wOoOoo

--- Bethany Miller - hangman project.py
+++ Bethany Miller - hangman project.py
@@ -1,15 +1,34 @@
 import random
+import string
 word = ["impossible", "important", "cellphone", "wonderful",
             "Jack", "dog", "house", "bubblegum!", "Mark", "books"]
 print(word)
 guesses = 10
-guess_right = []
-letters_guess_wrong = []
+guesses_right = []
 guesses_made = []
 random_word = random.choice(word)
 playing = True
 hidden = list(random_word)
 
-while guesses_made < 0 and playing:
-    letter = input("wUt LeTtEr?")
-    if letter in random_word: guess_right
+for i in range(len(hidden)):
+    if hidden[i] in string.ascii_letters:
+        hidden.pop(i)
+        hidden.insert(i, "_ ")
+
+while guesses_made != 0 and playing:
+    letter = input("wUt LeTtEr?").lower()
+    if letter in guesses_made:
+        print("you already guesses this letter")
+    elif letter not in random_word.lower():
+        print("wrong letter be smart and try another letter")
+        guesses -= 1
+        guesses_made.append(letter)
+        print("".join(hidden))
+    else:
+        print("oh you are actually smart that shocking you guess the letter right!")
+        guesses_right.append(letter)
+        guesses_made.append(letter)
+        for i in range(len(random_word)):
+            if random_word[i].lower() in guesses_right:
+                hidden.pop(i)
+                hidden.insert(i, random_word[i])
