edd17ccac3cccf4bec82278352a48429bb265d0d
Redflare654 <42391767+Redflare654@users.noreply.github.com>
2019-02-06 14:06:54 -0800

    mao

--- Dictionary notes.py
+++ Dictionary notes.py
@@ -36,26 +36,37 @@ print(nested_dictionary["NY"]["POPULATION"])
 complex_dictionary = {
     "CA": {
         "NAME": "California",
-        "POPULATION": 39500000 # 39,500,000
-    },
-    "NJ": {
-        "NAME": "New Jersey",
-        "POPULATION": 9000000, #9,000,000
+        "POPULATION": 39500000, # 39,500,000
         "CITIES":[
             "Fresno",
             "Los Banos",
             "Clovis"
         ]
     },
+    "NJ": {
+        "NAME": "New Jersey",
+        "POPULATION": 9000000, #9,000,000
+        "CITIES":[
+            "newark",
+            "trenton"
+        ]
+    },
     "WI": {
         "NAME": "Wisconsin",
         "POPULATION": 5800000, # 5,800,000
         "CITIES":[
-            
+            "Maddison",
+            "Milwaukee",
+            "Green bay"
         ]
     },
     "NY": {
         "NAME": "New york",
-        "POPULATION": 19500000 # 19,500,000
+        "POPULATION": 19500000, # 19,500,000
+        "CITIES":[
+            "New york City",
+            "Rockseter",
+            "Buffalo"
+        ]
     }
 }
