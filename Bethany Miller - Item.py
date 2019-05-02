1c3ca6ca055e3e401bb40d10d2309fa0ecb1ee38
Redflare654 <42391767+Redflare654@users.noreply.github.com>
2019-05-02 14:25:35 -0700

    NBETHJKBHYBKI

--- Bethany Miller - Item.py
+++ Bethany Miller - Item.py
@@ -3,6 +3,12 @@ class Item(object):
         self.name = name
 
 
+class Food(object):
+    def __init__(self, name):
+        super(Food, self).__init__(name)
+        self.energy = True
+
+
 class Flashlight(Item):
     def __init__(self, name):
         super(Flashlight, self).__init__(name)
@@ -21,3 +27,82 @@ class Chocolate(Item):
     def gives_energy(self):
         self.gives_energy = 5
         print("You eat de chocolate and it gives you energy and you are not hungry no more. yay!!!! you wont die now")
+
+
+class Weapon(Item):
+    def __init__(self, name, damage):
+        super(Weapon, self).__init__(name)
+        self.damage = damage
+
+
+class Armor(Item):
+    def __init__(self, name, armor_amt):
+        super(Armor, self).__init__(name)
+        self.armor_amt = armor_amt
+
+
+class Phone(Item):
+    def __init__(self, name):
+        super(Phone, self).__init__(name)
+        self.power = True
+        self.battery_left = 100
+
+
+class Water(Food):
+    def __init__(self, name):
+        super(Water, self).__init__(name)
+        self.heat = -10
+
+
+class Rope(Item):
+    def __init__(self, name):
+        super(Rope, self).__init__(name)
+        self.strength = 100
+
+
+class Headphone(Item):
+    def __init__(self, name):
+        super(Headphone, self).__init__(name)
+        self.used = 57
+        self.working = True
+
+
+class Backpack(Item):
+    def __init__(self, name):
+        super(Backpack, self).__init__(name)
+        self.space = 35
+        self.water_holder = 2
+
+
+class Jacket(Item):
+    def __init__(self, name):
+        super(Jacket, self).__init__(name)
+        self.give_warmth = True
+
+    def give_warmth(self):
+        self.give_warmth = 54
+        print("you put on a jacket you wont freeze now")
+
+
+class Gloves(Item):
+    def __init__(self, name):
+        super(Gloves, self).__init__(name)
+        self.protection = True
+
+
+class Ball(Item):
+    def __init__(self, name):
+        super(Ball, self).__init__(name)
+        self.bounce = 100
+
+
+class Watch(Item):
+    def __init__(self, name):
+        super(Watch, self).__init__(name)
+        self.battery = 150
+
+
+class Necklace(Item):
+    def __init__(self, name):
+        super(Necklace, self).__init__(name)
+        self.beauty = 75
--- Vaildator - Bethany Miller.py
+++ Vaildator - Bethany Miller.py
@@ -9,9 +9,15 @@ def validate(num: str):
 
 if test_num == test_num:
     string = "test_num"
-    list1 = list(test_num)
+    num_list = list(test_num)
+num_list.pop(15)
+print(num_list)
 
 
+def reverse_it(string1):
+    string1 = string1[::-1]
+    return string1
 
-print(list1)
 
+print(reverse_it(num_list))
+print(validate(test_num))
