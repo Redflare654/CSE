fd279dbac0bd758bf22f5558ec5cb5b75cea3ee4
Redflare654 <42391767+Redflare654@users.noreply.github.com>
2019-05-08 14:38:34 -0700

    awsdrftgyhjnm

--- Sales records - Bethany Miller.py
+++ Sales records - Bethany Miller.py
@@ -3,3 +3,15 @@ import csv
 
 with open("Sales Records.csv", 'r') as csv_file:
     reader = csv.reader(csv_file)
+#   with open("Book1.csv", 'r') as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         print("Writing file...   ")
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         for row in reader:
+#             old_number = int(row[0])
+#             new_number = old_number + 1
+#             row[0] = new_number
+#             writer.writerow(row)
+#             # print(int(old_number) + 1)
+#             # print(old_number)
