949eaeaf2dec2d62e5d7965d739c0c28b5711e0d
Redflare654 <42391767+Redflare654@users.noreply.github.com>
2019-05-10 14:48:31 -0700

    sdfgh

--- Sales records - Bethany Miller.py
+++ Sales records - Bethany Miller.py
@@ -3,9 +3,13 @@ import csv
 
 with open("Sales Records.csv", 'r') as csv_file:
     reader = csv.reader(csv_file)
+    total = 0
     for row in reader:
         profit = row[13]
         category = row[2]
         print(profit)
         if category == "Fruits":
-            print(profit, category)
+            profit = float(profit)
+            total += profit
+print(round(total, 2))
+
