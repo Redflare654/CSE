7701f18d390dd17af5b0c11ef1455e442c5553f0
Redflare654 <42391767+Redflare654@users.noreply.github.com>
2019-05-14 14:38:22 -0700

    3ertyhj

--- Sales records - Bethany Miller.py
+++ Sales records - Bethany Miller.py
@@ -4,12 +4,29 @@ import csv
 with open("Sales Records.csv", 'r') as csv_file:
     reader = csv.reader(csv_file)
     fruit_total = 0
+    clothes_total = 0
+    snacks_total = 0
+    meat_total = 0
     for row in reader:
         profit = row[13]
         category = row[2]
         print(profit)
         if category == "Fruits":
-            profit = float(profit)
-            fruit_total += profit
+            strawberry = float(profit)
+            fruit_total += strawberry
+        if category == "Clothes":
+            pants = float(profit)
+            clothes_total += pants
+        if category == "Snacks":
+            gummy = float(profit)
+            snacks_total += gummy
+        if category == "Meat":
+            beef = float(profit)
+            meat_total += beef
+        if category == "Cosmetics":
+            Cosmetics = float(profit)
+            
 print(round(fruit_total, 2))
+print(round(clothes_total, 2))
+print(round(snacks_total, 2))
 
